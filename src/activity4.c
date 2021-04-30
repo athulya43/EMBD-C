618b8b12d7b97b914b909bd03cfc8e37a9a711d2
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-30 22:22:12 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -1,3 +1,13 @@
+/**
+ * @file act1.c
+ * @author athulya43(you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<avr/io.h>
 #include"activity4.h"
 
