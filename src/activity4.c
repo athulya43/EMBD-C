e68c953fa4f41355c5109c6eb09dcf59d95610fe
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-30 22:21:37 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -1,1 +1,29 @@
+#include<avr/io.h>
+#include"activity4.h"
 
+void USARTInit(uint16_t ubrr_value)
+{
+
+    UBRR0L = ubrr_value;
+    UBRR0H = (ubrr_value>>8)&(0x00ff);
+    UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+
+}
+
+void USARTwriteChar(char c)
+{
+    while(!(UCSR0A & (1<<UDRE0)))
+    {
+
+    }
+    UDR0 = c;
+}
+char USARTREADChar()
+{
+  while(!(UCSR0A&(1<<RXC0)))
+  {
+  }
+  return 0;
+}
