ab5c00ad040a5349e08bb98ab4bf24d23c5b8387
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-30 22:20:22 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,1 +1,11 @@
+#ifndef ACTIVITY4_H_INCLUDED
+#define ACTIVITY4_H_INCLUDED
 
+void USARTInit(uint16_t ubrr_value);
+  
+void USARTwriteChar(char c);
+
+char UARTREADChar();
+
+
+#endif // ACTIVITY4_H_INCLUDED
