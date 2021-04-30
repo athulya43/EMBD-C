204a458c16a63a929883912ea51fb8d77f38f589
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-30 20:46:40 +0530

    Update activity3.c

--- src/activity3.c
+++ src/activity3.c
@@ -1,3 +1,13 @@
+/**
+ * @file act1.c
+ * @author athulya43(you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<util/delay.h>
 #include<avr/io.h>
 
@@ -8,17 +18,19 @@
  */
 int main(void)
 {
-char OutPWM (uint16_t temperature)
-{
+ char OutPWM (uint16_t temperature)
+  {
    char temp;
 
-    if((temperature>=0) && (temperature<=209)){
+   if((temperature>=0) && (temperature<=209))
+    {
 
         OCR1A = 205; //20% duty cycle
         temp = 20;
         _delay_ms(20);
     }
-    else if((temperature>=210) && (temperature<=509)){
+    else if((temperature>=210) && (temperature<=509))
+    {
 
         OCR1A = 410; //40% duty cycle
         temp = 25;
@@ -30,19 +42,21 @@ char OutPWM (uint16_t temperature)
         OCR1A = 717;//70% duty cycle
         temp = 29;
         _delay_ms(20);
-    }
-    else if((temperature>=710) && (temperature<=1024)){
+        }
+    else if((temperature>=710) && (temperature<=1024))
+        {
 
         OCR1A = 973; //95% duty cycle
         temp = 33;
         _delay_ms(20);
-    }
-    else{
+        }
+    else
+        {
         OCR1A = 0; //0% output
         temp = 0;
-    }
-    return temp;
-}
+        }
+     return temp;
+   }
 return 0;
 }
 
