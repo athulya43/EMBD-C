b5ea54cffaf0d0e69d06fc3703eaadb1f6ea5cdd
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-30 18:37:12 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,3 +1,13 @@
+/**
+ * @file act1.c
+ * @author athulya43(you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
 #include<util/delay.h>
 int main(void)
