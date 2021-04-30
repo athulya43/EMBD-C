d4f368737fea65fb4672fa61a05c4721445bec9b
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-30 11:53:58 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,1 +1,19 @@
+#include <avr/io.h>
 
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+
+
+void initADC();
+
+/** \brief Reads the temperature to a channel on MCU
+ *
+ * \param ch is the channel number in which the conversion takes place
+ * \param
+ * \return It returns a 16 bit number
+ *
+ */
+
+uint16_t Read_TEMP(uint8_t ch);
+
+#endif // ACTIVITY2_H_INCLUDED
