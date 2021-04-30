52b61cbf3e21a2fa9d29b9ec034592dabdd6c4d9
athulya43 <74306039+athulya43@users.noreply.github.com>
2021-04-30 11:52:10 +0530

    Update activity2.c

--- src/activity2.c
+++ src/activity2.c
@@ -1,1 +1,26 @@
+#include <avr/io.h>
+int main(void)
+{
+  void initADC()
+   {
+    ADMUX |= (1<<REFS0);   // for Aref = AVcc
+    ADCSRA |= (1<<ADEN)|(7<<ADPS0); //enable ADC and set prescalar to 128
+   }
+
+    uint16_t Read_TEMP(uint8_t ch)
+  {
+    // Select ADC channel ch must be 0-7
+    ADMUX&=0xf8; // clear the 3 LSBs for channel
+    ch&=0b00000111;
+    ADMUX|=ch;
+    // Start Single conversion
+    ADCSRA|=(1<<ADSC);
+    //Wait for conversion to complete
+    while(!(ADCSRA&(1<<ADIF)));
+    //Clear ADIF by writing one to it
+    ADCSRA|=(1<<ADIF);
+    return(ADC);
+  }
+ return 0;
+}
 
